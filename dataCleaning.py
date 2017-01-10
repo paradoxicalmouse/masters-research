5f3007f143cffa25eabc4ccc830c1409443b8a85
Taylor Morris <tbmorris@mtu.edu>
2017-01-10 14:36:06 -0500

    Changed data cleaning to be a method.

--- dataCleaning.py
+++ dataCleaning.py
@@ -7,49 +7,48 @@
 # Modification Notes: Added comments             #
 ##################################################
 
-# import statements
-import csv
-import nltk
-from nltk.tokenize import TweetTokenizer
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
+def cleanData ( 
+    filename = 'Questions.csv',
+    stopindex = 20,
+    removeEnglishStopWords = True,
+    removeHTMLTags = False,
+    stemTokens = True ):
 
-# create stemmer object
-ps = PorterStemmer()
+    # import statements
+    import csv
+    import nltk
+    from nltk.tokenize import TweetTokenizer
+    from nltk.corpus import stopwords
+    from nltk.stem import PorterStemmer
 
-# set stop words to english default
-stop_words = set(stopwords.words('english'))
+    # create stemmer object
+    ps = PorterStemmer()
 
-# create tokenizer object. Here, tweet tokenizer is used, as tweets are
-# similar in overall formal (html) to stack overflow postings.
-tknzr = TweetTokenizer()
+    # set stop words to english default
+    stop_words = set(stopwords.words('english'))
 
-# create filename string - just want to try and keep the code looking
-# clean 
-filename = '/Users/tbmorris/Documents/Current Working Assets/stacksample/Questions.csv'
+    # create tokenizer object. Here, tweet tokenizer is used, as tweets are
+    # similar in overall formal (html) to stack overflow postings.
+    tknzr = TweetTokenizer()
 
-# for testing purposes, we don't want to run over all of the data - that
-# would take much too long.
-stopindex = 20
-
-
-with open(filename,'rt') as csvfile: # open file
-    reader = csv.reader(csvfile, delimiter=',') # create file reader
-    i = 0 # create a counter to keep track of how many entries we've done
-    for row in reader: # for each entry in the data
-        if  i < stopindex: 
-            i += 1 
+    with open(filename,'rt') as csvfile: # open file
+        reader = csv.reader(csvfile, delimiter=',') # create file reader
+        i = 0 
+        # create a counter to keep track of how many entries we've done
+        for row in reader: # for each entry in the data
+            if  i < stopindex: 
+                i += 1 
     
-            # tokenize the entry's question text (description)
-            tokens =  tknzr.tokenize(row[6])
+                # tokenize the entry's question text (description)
+                tokens =  tknzr.tokenize(row[6])
 
-            # remove stop words
-            tokens = [w for w in tokens if not w in stop_words] 
+                # remove stop words
+                tokens = [w for w in tokens if not w in stop_words] 
             
-            # take tokens down to their stems
-            tokens = [ps.stem(w) for w in tokens]
+                # take tokens down to their stems
+                tokens = [ps.stem(w) for w in tokens]
 
-            # print the tokens for verification that it worked.
-            print(tokens)
-        else: # stop once we hit stopindex entries
-            break 
+                # print the tokens for verification that it worked.
+                print(tokens)
+            else: # stop once we hit stopindex entries
+                break 
