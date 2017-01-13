187e81b1b62afea78a35b624fef1094e7bf96dde
Taylor Morris <tbmorris@mtu.edu>
2017-01-13 12:31:47 -0500

    I forgot to commit yesterday.

--- dataCleaning.py
+++ dataCleaning.py
@@ -31,6 +31,9 @@ def cleanData (
     # similar in overall formal (html) to stack overflow postings.
     tknzr = TweetTokenizer()
 
+    # create list to return
+    cleanedData = []
+
     with open(filename,'rt') as csvfile: # open file
         reader = csv.reader(csvfile, delimiter=',') # create file reader
         i = 0 
@@ -38,19 +41,23 @@ def cleanData (
         for row in reader: # for each entry in the data
             if  i < stopindex: 
                 i += 1 
-    
+                
+                if i == 1:
+                    continue 
                 # tokenize the entry's question text (description)
                 tokens =  tknzr.tokenize(row[6])
 
                 # remove stop words
                 if removeEnglishStopWords:
-                    tokens = [w for w in tokens if not w in stop_words] 
-            
+                    tokens = (
+                        [w.lower() for w in tokens if not w in stop_words] 
+                    )
                 # take tokens down to their stems
                 if stemTokens:
                     tokens = [ps.stem(w) for w in tokens]
 
                 # print the tokens for verification that it worked.
-                print(tokens)
+                cleanedData.append(tokens)
             else: # stop once we hit stopindex entries
                 break 
+    return cleanedData
