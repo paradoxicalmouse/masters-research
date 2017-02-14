a976a623d189fcb9be4d8337303b7e804b54f46c
Taylor Morris <tbmorris@mtu.edu>
2017-02-14 14:51:58 -0500

    Working on cleaning questionsCorp for body data - issue with stemming. Likely due to the periods within terms.

--- r-research/questions.r
+++ r-research/questions.r
@@ -8,13 +8,11 @@ questions <- read.table("../Questions.csv",
                         nrows=10)
 require(tm)
 questionsCorp <- Corpus(VectorSource(questions$Body))
-meta(questionsCorp, tag = "Id")<-questions$Id
-meta(questionsCorp, tag = "OwnerUserId")<-questions$OwnerUserId
-meta(questionsCorp, tag = "CreationDate")<-questions$CreationDate
-meta(questionsCorp, tag = "ClosedDate")<-questions$ClosedDate
-meta(questionsCorp, tag = "Score")<-questions$Score
-meta(questionsCorp, tag = "Title")<-questions$Title
-
+questionsCorp <- tm_map(questionsCorp, stripWhitespace)
+questionsCorp <- tm_map(questionsCorp, content_transformer(tolower))
+questionsCorp <- tm_map(questionsCorp, removeWords, stopwords("english"))
+#tm_map(questionsCorp, stemDocument)
 questionsDTM <- DocumentTermMatrix(questionsCorp)
+questionsFrame <- data.frame(as.matrix(questionsDTM))
 #http://stackoverflow.com/questions/21163207/document-term-matrix-for-naive-bayes-classfier-unexpected-results-r
 #may help from here.
