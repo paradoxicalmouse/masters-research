db869c2cd19266028c17efe9641d326452094a64
Taylor Morris <tbmorris@mtu.edu>
2017-02-14 16:22:01 -0500

    Snowball added, meeting 2/14

--- r-research/questions.r
+++ r-research/questions.r
@@ -5,14 +5,20 @@ questions <- read.table("../Questions.csv",
                         comment.char="",
                         allowEscapes = F,
                         quote='"',
-                        nrows=10)
+                        nrows=20)
 require(tm)
+require(SnowballC)
 questionsCorp <- Corpus(VectorSource(questions$Body))
-questionsCorp <- tm_map(questionsCorp, stripWhitespace)
+titlesCorp <- Corpus(VectorSource(questions$Title))
+#ftp://cran.r-project.org/pub/R/web/packages/tm/vignettes/tm.pdf
+#questionsCorp <- tm_map(questionsCorp, stripWhitespace)
 questionsCorp <- tm_map(questionsCorp, content_transformer(tolower))
 questionsCorp <- tm_map(questionsCorp, removeWords, stopwords("english"))
-#tm_map(questionsCorp, stemDocument)
-questionsDTM <- DocumentTermMatrix(questionsCorp)
+questionsCorp <- tm_map(questionsCorp, removePunctuation)
+questionsCorp <- tm_map(questionsCorp, stemDocument)
+questionsDTM <- DocumentTermMatrix(questionsCorp, control=list(removePunctuation=TRUE))
+#questionsDTM <- inspect(removeSparseTerms(questionsDTM, 0.9))
 questionsFrame <- data.frame(as.matrix(questionsDTM))
+names(questionsFrame)
 #http://stackoverflow.com/questions/21163207/document-term-matrix-for-naive-bayes-classfier-unexpected-results-r
 #may help from here.
